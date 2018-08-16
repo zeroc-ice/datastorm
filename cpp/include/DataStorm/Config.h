4c18d65a2bdd8ed6633a3a4fef443b74f4458cab
Benoit Foucher <benoit@zeroc.com>
2018-08-16 18:12:08 +0200

    Disable C4503 warning for now

--- cpp/include/DataStorm/Config.h
+++ cpp/include/DataStorm/Config.h
@@ -45,3 +45,8 @@
 #if defined(_MSC_VER) && !defined(DATASTORM_BUILDING_DATASTORM)
 #   pragma comment(lib, DATASTORM_LIBNAME("DataStorm"))
 #endif
+
+//
+// Disable some warnings
+// TODO: find a better solution
+#pragma warning(disable:4503) // decorated name length exceeded, name was truncated
