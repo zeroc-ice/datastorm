45b2cb79eb9563975fca18a2f1aa9f6754f9d21c
Benoit Foucher <benoit@zeroc.com>
2018-09-07 17:28:50 +0200

    Fixed headers

--- allTests.py
+++ allTests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # **********************************************************************
 #
-# Copyright (c) 2018 ZeroC, Inc. All rights reserved.
+# Copyright (c) 2018-present ZeroC, Inc. All rights reserved.
 #
 # **********************************************************************
 
